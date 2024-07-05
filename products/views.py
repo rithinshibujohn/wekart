26e9d95184ddd3aae7afce1875f6a8f0ef83647f
rithinshibujohn <125265236+rithinshibujohn@users.noreply.github.com>
2024-07-05 16:07:41 +0530

    05-07-2024 @ 4PM Video Eps Part 23 4

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from . models import Product
 
 # Create your views here.
 def index(request):
@@ -14,7 +15,9 @@ def list_product(request):
     Returns:
     _type_: _description_
     """
-    return render(request,'products.html')
+    product_list=Product.objects.all()
+    context={'products':product_list}
+    return render(request,'products.html',context)
 
 def detail_product(request):
     return render(request,'product_details.html')
--- wekartproject/urls.py
+++ wekartproject/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
      path('',include('products.urls')),
-     path('customers/',include('customers.urls'))
+     path('customers/',include('customers.urls')),
+     path('orders/',include('orders.urls'))
 
 
 ]
