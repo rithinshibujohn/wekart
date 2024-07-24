3d908954402802801c1e3a694857ea3f8fa276ba
rithinshibujohn <rithinshibujohn@gmail.com>
2024-07-24 09:54:12 +0530

    24-07-24 @ 10AM

--- products/views.py
+++ products/views.py
@@ -3,9 +3,14 @@ from . models import Product
 from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
-    featured_product=Product.objects.order_by('-priority-')[:4]
-    latest_product=Product.objects.order_by('-priority-')[:4]
-    return render(request,'index.html')
+    featured_products=Product.objects.order_by('priority')[:4]
+    latest_products=Product.objects.order_by('-id')[:4]
+    context={   
+             'featured_products':featured_products,
+             'latest_products':latest_products
+    }
+    print(context)
+    return render(request,'index.html',context)
 
 def list_product(request):
     """_summary_
