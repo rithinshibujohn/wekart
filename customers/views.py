cd3125c0f4abe98145f867f2f7817da1bf237971
rithinshibujohn <rithinshibujohn@gmail.com>
2024-07-31 11:57:15 +0530

    31-07-24 @11.57am

--- customers/views.py
+++ customers/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 from . models import Customer
-
+def sign_out(request):
+    logout(request)
+    return redirect('home')
+    
 # Create your views here.
 def show_account(request):
     if request.POST and 'register' in request.POST:
@@ -13,13 +17,13 @@ def show_account(request):
             address=request.POST.get('address')
             phone=request.POST.get('phone')
             #create user accounts 
-            user=User.objects.create(
+            user=User.objects.create_user(
                 username=username,
                 password=password,
                 email=email
             )
             # create customer account
-            customer=Customer.objects.create(
+            customer=Customer.objects.create_user(
                 user=user,
                 phone=phone,
                 address=address
@@ -27,5 +31,16 @@ def show_account(request):
             return redirect('home')
         except Exception as e: 
             error_message="Duplicate username or invalid credentials"
-            messages.error(request,error_message)            
+            messages.error(request,error_message)  
+    if request.POST and 'login' in request.POST:
+        username=request.POST.get('username')
+        password= request.POST.get('password')
+        user=authenticate(username=username,password=password)
+        print('user while login\n',user)
+        if user:
+            login(request,user)
+            print('login successsfull')
+            return redirect('home')
+        else:
+            messages.error(request,"Invalid User Credentials")
     return render(request,'account.html')
